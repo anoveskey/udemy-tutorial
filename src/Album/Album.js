b05813dc9bd29780b6bf977c05c1719d6ced6ea5
Adam Noveskey <anoveskey@nexient.com>
2019-04-08 10:53:14 -0400

    Saving state before using the eject command

--- src/Album/Album.js
+++ src/Album/Album.js
@@ -3,8 +3,8 @@ import './Album.css';
 
 const Album = (props) => {
     return (
-        <div className="album" onClick={props.click}>
-            <h4>{props.title}</h4>
+        <div className="album">
+            <h4 onClick={props.click}>{props.title}</h4>
             <h5>by {props.artist}</h5>
             <input type="text" onChange={props.changed} value={props.title}/>
         </div>
--- src/App.js
+++ src/App.js
@@ -1,65 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Album from './Album/Album';
 
-class App extends Component {
-    state = {
-        albums: [
-            {artist: "The Beach Boys", title: "Pet Sounds"},
-            {artist: "Syd Barrett", title: "The Madcap Laughs"},
-            {artist: "Chemlab",title: "Eastside Militia"}
-            ],
-        showAlbums: false
-    };
-    
-    switchCurrentListeningHandler = (differentTitle) => {
-        this.setState({
-        albums: [
-            {artist: "The Beach Boys", title: "Pet Sounds"},
-            {artist: "Syd Barrett", title: "The Madcap Laughs"},
-            {artist: "Chemlab",title: differentTitle}
-        ]
-    })
-    }
 
-    nameChangedHandler = (event) => {
-        this.setState({
-            albums: [
-                {artist: "The Beach Boys", title: "Pet Sounds"},
-                {artist: "Syd Barrett", title: "The Madcap Laughs"},
-                {artist: "Chemlab",title: event.target.value},
-                {artist: "The Cure", title:"Faith"}
-            ]
-        })
-    }
-    toggleAlbumsHandler = () => {
-        const doesShow = this.state.showAlbums
-        this.setState({ showAlbums: !doesShow })
-    }
-  render() {
-        return (
-          <div className="App">
-              <button onClick={this.toggleAlbumsHandler}>Show albums!</button>
-              { this.state.showAlbums ? <div>
-                  <Album
-                      title={this.state.albums[0].title}
-                      artist={this.state.albums[0].artist}
-                      click={this.switchCurrentListeningHandler.bind(this, 'Oxidizer')}/>
-                  <Album
-                      title={this.state.albums[1].title}
-                      artist={this.state.albums[1].artist} />
-                  <Album
-                      title={this.state.albums[2].title}
-                      artist={this.state.albums[2].artist}
-                      changed={this.nameChangedHandler}/>
-                  { this.state.albums.length > 3 ? <Album
-                      title={this.state.albums[3].title}
-                      artist={this.state.albums[3].artist}/> : null
-                  }
-              </div> : null }
-          </div>
-      );
-  }
-  }
+const App = (props) => {
+            const style = {
+                backgroundColor: 'darkBlue',
+                color: 'white',
+                padding: '5px',
+            }
+            const [albumsState, setAlbumsState] = useState( {
+                albums: [
+                    {id: 'asdf', artist: "The Beach Boys", title: "Pet Sounds"},
+                    {id: 'ghjk', artist: "Syd Barrett", title: "The Madcap Laughs"},
+                    {id: 'rtyu', artist: "Chemlab",title: "Eastside Militia"}
+                ],
+                showAlbums: false
+            })
+            const nameChangedHandler = (event, id) => {
+                const albumIndex = albumsState.albums.findIndex(a => {
+                    return a.id === id;
+                });
+                const album = Object.assign({}, albumsState.albums[albumIndex]);
+                album.title = event.target.value;
+                const albums = [...albumsState.albums];
+                albums[albumIndex] = album;
+                setAlbumsState({
+                    albums: albums,
+                    showAlbums: albumsState.showAlbums
+                })
+            }
+            const deleteAlbumHandler = (albumIndex) => {
+                const albums = [...albumsState.albums];
+                albums.splice(albumIndex, 1);
+                setAlbumsState({albums: albums, showAlbums: albumsState.showAlbums});
+            }
+            const toggleAlbumsHandler = () => {
+                const doesShow = albumsState.showAlbums
+                setAlbumsState({
+                    albums: [
+                        {id: 'asdf', artist: "The Beach Boys", title: "Pet Sounds"},
+                        {id: 'ghjk', artist: "Syd Barrett", title: "The Madcap Laughs"},
+                        {id: 'qwer', artist: "Chemlab",title: "10 Ton Pressure"},
+                        {id: 'zxcv', artist: "The Cure", title:"The Head On The Door"}
+                    ],
+                    showAlbums: !doesShow })
+            }
+            let albums = null;
+
+            if (albumsState.showAlbums) {
+                albums = (
+                    <div>
+                    {albumsState.albums.map((album, index) => {
+                        return (<Album
+                            key={album.id}
+                            click={() => deleteAlbumHandler(index)}
+                            title={album.title}
+                            artist={album.artist}
+                            changed={(event) => nameChangedHandler(event, album.id)}/>)
+                    })}
+                    </div>
+                );
+                style.backgroundColor = 'blue';
+            }
+            return (
+                <div className="App">
+                    <button style={style} onClick={toggleAlbumsHandler}>Show albums!</button>
+                    {albums}
+                </div>
+            );
+        }
 
 export default App;
